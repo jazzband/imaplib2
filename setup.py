b3fdd75a9562b8f0d9c3ad27e4ba5a249f66f4b5
bcoe <bencoe@gmail.com>
2014-04-24 14:39:45 -0700

    bump release #.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="imaplib2",
-    version="2.28.2",
+    version="2.28.3",
     description="A threaded Python IMAP4 client.",
     author="Piers Lauder",
     url="http://github.com/bcoe/imaplib2",
